092b6f18afe658625f916abf4de1d62d205a504e
MatejJanecek <janecma@student.uzlabina.cz>
2020-05-18 04:31:33 +0200

    Initial commit

---
+++ Rekurze.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int faktorial(int); /*Definice funkce*/
+
+int main() /*Tělo programu*/
+{
+	int x, y;
+	printf("Vlozte parametr: ");
+	scanf_s("%i", &x);
+    faktorial(x);
+	printf("Faktrorial je: %i", faktorial(x));
+	return 0;
+}
+
+int faktorial(int a) /*Tělo funkce faktoriál*/
+{
+	int nic = 1;
+
+	while (a > 1)
+	{
+		nic = nic * a;
+		a--;
+	}
+
+	return nic;
+}
+
